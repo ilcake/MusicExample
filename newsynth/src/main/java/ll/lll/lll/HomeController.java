5d7cb4cacf5caea61c7c8ab8b0c46e61100ce499
ilcake <ilcake@My>
2017-04-14 09:41:42 +0900

    newBranch

--- newsynth/src/main/java/ll/lll/lll/HomeController.java
+++ newsynth/src/main/java/ll/lll/lll/HomeController.java
@@ -1,15 +1,20 @@
 package ll.lll.lll;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ll.lll.lll.javaCC.*;
+import ll.lll.lll.jv.*;
 
 /**
  * Handles requests for the application home page.
@@ -78,6 +83,14 @@ public class HomeController {
 
 	@RequestMapping(value = "/sample8", method = RequestMethod.GET)
 	public String sample8() {
+
+		return "sample8";
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/compile", method = RequestMethod.GET)
+	public String compile(String source) {
+		System.out.println(source);
 
 		return "sample8";
 	}
